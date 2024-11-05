fb79fb24cb68a1cf768e800c13e093a6372c29a6
unknown <varankou@yandex.ru>
2024-11-05 08:47:47 +0300

    chapter 5 of "Practical Statistic for Data Scientists"

--- practical_statistics_for_data_scientists/script/chapter_5.R
+++
@@ -1,6 +0,0 @@
-# this file contains the worked examples and solutions to the problems from Chapter 5 of
-# Bruce, P., Bruce, A. 2017. "Practical Statistics for Data Scientists", O'Reilly.
-
-# ---------------
-# pages [220; )
-# basics of classification
